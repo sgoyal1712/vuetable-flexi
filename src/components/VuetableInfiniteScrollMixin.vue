54f679b30d5779262ec6d81a6bdffeed10226d43
tarun <tarung1793@gmail.com>
2019-03-07 10:51:53 +0530

    InfiniteScroll: handle case where first page load is insufficient to fill the screen

--- src/components/VuetableInfiniteScrollMixin.vue
+++ src/components/VuetableInfiniteScrollMixin.vue
@@ -4,7 +4,10 @@
       return {
         isLoadingData: false,
         rowElemHeight: 0,
-      }
+        lastClientHeight: 0,
+        lastEventTarget: null,
+        tBody: null,
+      };
     },
     props: {
       infiniteScroll: {
@@ -13,37 +16,56 @@
       },
       numRowsToBottom: {
         type: Number,
-        default: 10,
+        default: 5,
       },
     },
     methods: {
-      handleInfiniteScroll(e) {
-        const { clientHeight, scrollTop, scrollHeight } = e.target
-        if (this.isLoadingData) return
+      handleInfiniteScroll({ target }) {
+        const { clientHeight, scrollTop, scrollHeight } = target;
+        if (this.isLoadingData || this.hasNoMoreData) return;
         if (clientHeight + scrollTop > scrollHeight - (this.numRowsToBottom * this.rowElemHeight)) {
-          this.isLoadingData = true
-          this.gotoNextPage()
+          this.lastClientHeight = scrollTop;
+          this.lastEventTarget = target;
+          this.gotoNextPage();
         }
       },
       dataReceived() {
-        this.isLoadingData = false
+        if (this.lastEventTarget) { this.lastEventTarget.scrollTop = this.lastClientHeight - 50; }
+        this.isLoadingData = false;
+        this.lastEventTarget = null;
+        // the below call is required for the case where the first
+        // page is insufficient to fill the whole table. No scrollbar is rendered
+        // and hence the scroll event is never fired
+        this.$nextTick(() => {
+          this.handleInfiniteScroll({ target: this.tBody });
+        });
+      },
+      makingDataCall() {
+        this.isLoadingData = true;
+      },
+    },
+    computed: {
+      hasNoMoreData() {
+        if (!this.tablePagination) return true;
+        return this.tableData.length === this.tablePagination.total;
       },
     },
     mounted() {
-      if (!this.isFixedHeader) return
-      const tBody = this.$el.getElementsByClassName('vuetable-body-wrapper')[0]
-      const rowElem = this.$el.querySelectorAll('tbody.vuetable-body tr')[0]
-      if (!tBody || !rowElem) return
-      this.rowElemHeight = rowElem.clientHeight
-      tBody.addEventListener('scroll', this.handleInfiniteScroll)
-      this.$on('vuetable:loaded', this.dataReceived)
+      if (!this.isFixedHeader || !this.infiniteScroll) return;
+      const rowElem = this.$el.querySelectorAll('tbody.vuetable-body tr')[0];
+      [this.tBody] = this.$el.getElementsByClassName('vuetable-body-wrapper');
+      if (!this.tBody || !rowElem) return;
+      this.rowElemHeight = rowElem.clientHeight;
+      this.tBody.addEventListener('scroll', this.handleInfiniteScroll);
+      this.$on('vuetable:loaded', this.dataReceived);
+      this.$on('vuetable:loading', this.makingDataCall);
     },
     destroyed() {
-      const tBody = this.$el.getElementsByClassName('vuetable-body-wrapper')[0]
-      if (tBody != null) {
-        tBody.removeEventListener('scroll', this.handleInfiniteScroll)
-        this.$off('vuetable:loaded', this.dataReceived)
+      if (this.tBody != null) {
+        this.tBody.removeEventListener('scroll', this.handleInfiniteScroll);
+        this.$off('vuetable:loaded', this.dataReceived);
+        this.$off('vuetable:loading', this.makingDataCall);
       }
     },
-  }
+  };
 </script>
